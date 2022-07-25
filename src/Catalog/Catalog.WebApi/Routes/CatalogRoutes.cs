ca0ae40119ff7217726e6ce4e7a7db9ec90c5ebf
Ivan Mladenov <ivan.mladenov@concordeu.com>
2022-07-25 17:05:36 +0300

    Implement rest of routes

--- src/Catalog/Catalog.WebApi/Routes/CatalogRoutes.cs
+++ src/Catalog/Catalog.WebApi/Routes/CatalogRoutes.cs
@@ -1,4 +1,5 @@
 ﻿using Catalog.WebApi.Data;
+using Catalog.WebApi.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog.WebApi.Routes
@@ -7,12 +8,38 @@ namespace Catalog.WebApi.Routes
     {
         public static WebApplication AddCatalogRoutes(this WebApplication application)
         {
+            const string BaseRoute = "/api/Products";
+
             application
-                .MapGet("/api/Products", GetAllProducts);
+                .MapGet(BaseRoute,
+                async ([FromServices] ICatalog catalog) => await catalog.GetProducts());
+
+            application
+                .MapGet(BaseRoute + "/{id}",
+                async (string id, [FromServices] ICatalog catalog) => await catalog.GetProduct(id) is Product product ?
+                Results.Ok(product) : Results.NotFound());
+
+            application
+                .MapGet(BaseRoute + "/ByCategory/{category}",
+                async (string category, [FromServices] ICatalog catalog) => await catalog.GetProductByCategory(category));
+
+            application
+                .MapGet(BaseRoute + "/ByName/{name}",
+                async (string name, [FromServices] ICatalog catalog) => await catalog.GetProductByName(name));
+
+            application
+                .MapPost(BaseRoute,
+                async ([FromBody] Product product, [FromServices] ICatalog catalog) => await catalog.CreateProduct(product));
+
+            application
+                .MapPut(BaseRoute,
+                async ([FromBody] Product updatedProduct, [FromServices] ICatalog catalog) => await catalog.UpdateProduct(updatedProduct));
+
+            application
+                .MapDelete(BaseRoute + "/{id}",
+                async (string id, [FromServices] ICatalog catalog) => await catalog.DeleteProduct(id));
 
             return application;
         }
-
-        private static async Task GetAllProducts([FromServices] ICatalog catalog) => await catalog.GetProducts();
     }
 }
