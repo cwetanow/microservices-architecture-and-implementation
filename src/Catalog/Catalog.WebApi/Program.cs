e6a738bbb145350ae68b082002b40dfee9f7abab
Ivan Mladenov <ivan.mladenov@concordeu.com>
2022-07-25 15:59:47 +0300

    Implement minimal api for get all

---
+++ src/Catalog/Catalog.WebApi/Program.cs
@@ -0,0 +1,34 @@
+using Catalog.WebApi.Data;
+using Catalog.WebApi.Routes;
+using Catalog.WebApi.Settings;
+using MongoDB.Driver;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var services = builder.Services;
+
+var settings = builder.Configuration.GetSection(nameof(DatabaseSettings)).Get<DatabaseSettings>();
+
+services.AddSingleton(_ => new MongoClient(settings.ConnectionString).GetDatabase(settings.DatabaseName));
+
+services.AddScoped<ICatalog, MongoCatalog>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app
+    .AddCatalogRoutes()
+    .Run();
---
+++ src/Catalog/Catalog.WebApi/Routes/CatalogRoutes.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.WebApi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.WebApi.Routes
+{
+    public static class CatalogRoutes
+    {
+        public static WebApplication AddCatalogRoutes(this WebApplication application)
+        {
+            application
+                .MapGet("/api/Products", GetAllProducts);
+
+            return application;
+        }
+
+        private static async Task GetAllProducts([FromServices] ICatalog catalog) => await catalog.GetProducts();
+    }
+}
