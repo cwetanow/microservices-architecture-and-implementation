cf56a99562434307a76d4c48f3ce8d598d0ec0b5
Ivan Mladenov <ivan.mladenov@concordeu.com>
2022-07-26 13:13:49 +0300

    Create entities

---
+++ src/Basket/Basket.WebApi/Entities/ShoppingCart.cs
@@ -0,0 +1,10 @@
+﻿namespace Basket.WebApi.Entities;
+
+public class ShoppingCart
+{
+    private readonly List<ShoppingCartItem> items = new();
+
+    public string User { get; } = string.Empty;
+
+    public IReadOnlyCollection<ShoppingCartItem> Items => items.AsReadOnly();
+}
