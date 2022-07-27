3bf56a522b266e81d9c621579bdeacfdf7651bf6
Ivan Mladenov <madritistaaa@gmail.com>
2022-07-27 17:22:49 +0300

    Create grpc service (#4)
    Co-authored-by: Ivan Mladenov <ivan.mladenov@concordeu.com>

---
+++ src/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,67 @@
+﻿using Discount.Domain.Data;
+using Discount.Grpc.Protos;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : Protos.DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountData data;
+
+        public DiscountService(IDiscountData data)
+        {
+            this.data = data;
+        }
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = new Domain.Entities.Coupon
+            {
+                Amount = request.Coupon.Amount,
+                Description = request.Coupon.Description,
+                ProductId = request.Coupon.ProductId,
+                Id = request.Coupon.Id
+            };
+
+            await data.CreateDiscount(coupon);
+
+            return request.Coupon;
+        }
+
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            await data.DeleteDiscount(request.ProductId);
+            return new DeleteDiscountResponse
+            {
+                Success = true
+            };
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var discount = await data.GetDiscount(request.ProductId);
+            return new CouponModel
+            {
+                ProductId = discount.ProductId,
+                Amount = (int)discount.Amount,
+                Description = discount.Description,
+                Id = discount.Id
+            };
+        }
+
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            var updatedCoupon = new Domain.Entities.Coupon
+            {
+                Amount = request.Coupon.Amount,
+                Description = request.Coupon.Description,
+                ProductId = request.Coupon.ProductId,
+                Id = request.Coupon.Id
+            };
+
+            await data.UpdateDiscount(updatedCoupon);
+
+            return request.Coupon;
+        }
+    }
+}
